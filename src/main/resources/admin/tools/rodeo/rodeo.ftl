cd963938f6b2398d377141911f0b07bb1af02338
Tom Arild Jakobsen <tajakobsen@gmail.com>
2024-08-19 15:43:10 +0200

    List all components in all applications

--- src/main/resources/admin/tools/rodeo/rodeo.ftl
+++ src/main/resources/admin/tools/rodeo/rodeo.ftl
@@ -1,3 +1,9 @@
+[#-- @ftlvariable name="parts" type="java.util.ArrayList" --]
+[#-- @ftlvariable name="layouts" type="java.util.ArrayList" --]
+[#-- @ftlvariable name="pages" type="java.util.ArrayList" --]
+[#-- @ftlvariable name="contentStudioUrl" type="String" --]
+[#-- @ftlvariable name="repo" type="String" --]
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -14,7 +20,7 @@
         <summary>${part.key} (${part.total})</summary>
         <ul>
           [#list part.contents as cont]
-            <li>${cont._path}</li>
+            <li><a href="${contentStudioUrl}/${cont.repo}/edit/${cont._id}">${cont._path}</a></li>
           [/#list]
         </ul>
       </details>
--- src/main/resources/admin/tools/rodeo/rodeo.ts
+++ src/main/resources/admin/tools/rodeo/rodeo.ts
@@ -1,42 +1,79 @@
 import { render } from "/lib/tineikt/freemarker";
-import { Content, query } from "/lib/xp/content";
+import { getToolUrl } from "/lib/xp/admin";
+import { query, type Content } from "/lib/xp/content";
+import { list as listApps } from "/lib/xp/app";
 import { run } from "/lib/xp/context";
+import { list as listRepos } from "/lib/xp/repo";
 import { listComponents, type LayoutDescriptor, type PageDescriptor, type PartDescriptor } from "/lib/xp/schema";
 
 const view = resolve("rodeo.ftl");
 
-export function all() {
-  const allParts = listComponents({
-    application: "no.seeds.fiskeridir",
-    type: "PART",
-  });
+export function all(): XP.Response {
+  const cmsRepos = listRepos()
+    .map((repo) => repo.id)
+    .filter((repoId) => startsWith(repoId, "com.enonic.cms"));
 
-  const allLayouts = listComponents({
-    application: "no.seeds.fiskeridir",
-    type: "LAYOUT",
-  });
+  const allParts = flatMap(listApps(), (app) =>
+    listComponents({
+      application: app.key,
+      type: "PART",
+    })
+  );
 
-  const allPages = listComponents({
-    application: "no.seeds.fiskeridir",
-    type: "PAGE",
-  });
+  const allLayouts = flatMap(listApps(), (app) =>
+    listComponents({
+      application: app.key,
+      type: "LAYOUT",
+    })
+  );
 
-  log.info(JSON.stringify(allPages, null, 2));
+  const allPages = flatMap(listApps(), (app) =>
+    listComponents({
+      application: app.key,
+      type: "PAGE",
+    })
+  );
+
+  const parts = allParts.map((component) => getComponentUsagesInRepo(component, cmsRepos));
+  const layouts = allLayouts.map((component) => getComponentUsagesInRepo(component, cmsRepos));
+  const pages = allPages.map((component) => getComponentUsagesInRepo(component, cmsRepos));
 
   return {
     body: render<FreemarkerParams>(view, {
-      parts: allParts.map((component) => getComponentUsages(component)),
-      layouts: allLayouts.map((component) => getComponentUsages(component)),
-      pages: allPages.map((component) => getComponentUsages(component)),
+      contentStudioUrl: getToolUrl("com.enonic.app.contentstudio", "main"),
+      parts,
+      layouts,
+      pages,
     }),
   };
 }
 
-function getComponentUsages(component: Component): Usages {
+function getComponentUsagesInRepo(component: Component, repositories: string[]): Usages {
+  return repositories
+    .map((repository) => getComponentUsages(component, repository))
+    .reduce<Usages>(
+      (usages, componentUsage) => {
+        return {
+          total: usages.total + componentUsage.total,
+          count: usages.count + componentUsage.total,
+          key: usages.key,
+          contents: usages.contents.concat(componentUsage.contents),
+        };
+      },
+      {
+        total: 0,
+        count: 0,
+        key: component.key,
+        contents: [],
+      }
+    );
+}
+
+function getComponentUsages(component: Component, repository: string): Usages {
   const res = run(
     {
       branch: "draft",
-      repository: "com.enonic.cms.default",
+      repository,
       principals: ["role:system.admin"],
     },
     () =>
@@ -50,13 +87,17 @@ function getComponentUsages(component: Component): Usages {
     total: res.total,
     count: res.count,
     key: component.key,
-    contents: res.hits,
+    contents: res.hits.map((hit) => ({
+      ...hit,
+      repo: stringAfterLast(repository, "."),
+    })),
   };
 }
 
 type Component = PartDescriptor | LayoutDescriptor | PageDescriptor;
 
 type FreemarkerParams = {
+  contentStudioUrl: string;
   parts: Usages[];
   layouts: Usages[];
   pages: Usages[];
@@ -66,5 +107,17 @@ type Usages = {
   key: string;
   total: number;
   count: number;
-  contents: Content<unknown>[];
+  contents: (Content<unknown> & { repo: string })[];
 };
+
+function startsWith(str: string, searchString: string): boolean {
+  return str.substring(0, searchString.length) === searchString;
+}
+
+function flatMap<A, B>(arr: A[], f: (val: A) => B[]): B[] {
+  return arr.reduce<B[]>((res, val) => res.concat(f(val)), []);
+}
+
+function stringAfterLast(str: string, delimiter: string): string {
+  return str.substring(str.lastIndexOf(delimiter) + 1);
+}
